9751a48a54462088060c22fe2cad57be3394c048
unknown <kemper_969@mail.ru>
2026-08-20 15:55:48 +0400

    update dags done for rub parser

--- dags/banks_rub.py
+++ dags/banks_rub.py
@@ -13,7 +13,7 @@ from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.utils.dates import days_ago
 
 
-DATA_PATH = '/opt/data_download'
+DATA_PATH = '/opt/airflow/data_download'
 
 default_args = {
  'owner': 'alex',
@@ -27,18 +27,20 @@ with DAG(
     schedule_interval = '30 7 * * *', # min hour month ( days week from 1-7 ) (+8 UTC по умл)
     description = 'Выгрузка и обработка банковских данных',
     start_date = days_ago(1) ,
+    catchup=False,
+    max_active_runs=1,
 ) as dag:
 
-    
+    #curl https://raw.githubusercontent.com/fawazahmed0/currency-api/1/latest/currencies/eur.json -s > /opt/airflow/data_download/raw/euro_{{ ds }}.json
     # Создание папок и загрузка json данными
     check_data_folder = BashOperator(
         task_id='fetch_data_from_api',
         bash_command = 
-        f"""
-        mkdir -p {DATA_PATH}/raw
-        mkdir -p {DATA_PATH}/clean
-        curl https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json -s > {DATA_PATH}/raw/euro_{{ ds }}.json
-        curl https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json -s > {DATA_PATH}/raw/usd_{{ ds }}.json
+        """
+        mkdir -p /opt/airflow/data_download/raw
+        mkdir -p /opt/airflow/data_download/clean
+        curl https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json -s > /opt/airflow/data_download/raw/euro_{{ ds }}.json
+        curl https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json -s > /opt/airflow/data_download/raw/usd_{{ ds }}.json
         """,
     )
 
@@ -46,30 +48,40 @@ with DAG(
         execution_data = context['ds']
 
         # EUR
-        raw_file_path = f'{DATA_PATH}/raw/euro_{execution_data}.json'
+        raw_file_path_eur = f'{DATA_PATH}/raw/euro_{execution_data}.json'
         clean_file_data = f'{DATA_PATH}/clean/euro_{execution_data}.parquet'
+        
 
         #raw_file_path_dol = f'/data/raw/usd_{execution_data}.json'
         #clean_data_dol = f'/data/clean/usd_{execution_data}.parquet'
 
-        if not os.path.exists(raw_file_path):
-            print(f"❌ Файл не найден: {raw_file_path}")
+        if not os.path.exists(raw_file_path_eur):
+            print(f"❌ Файл не найден: {raw_file_path_eur}")
             print(f"📂 Проверяем содержимое {DATA_PATH}/raw/:")
             if os.path.exists(f'{DATA_PATH}/raw/'):
                 for f in os.listdir(f'{DATA_PATH}/raw/'):
                     print(f"  - {f}")
             else:
                 print(f"❌ Папка {DATA_PATH}/raw/ не существует!")
-            raise FileNotFoundError(f"Файл не найден: {raw_file_path}")
+            raise FileNotFoundError(f"Файл не найден: {raw_file_path_eur}")
         
-        with open(raw_file_path,'r') as f:
-            data = json.load(f)
+        with open(raw_file_path_eur, 'r') as f:
+            data_eur = json.load(f)
 
-        df = pd.DataFrame(data)
-        df['date'] = execution_data # время
-        df.to_parquet(clean_file_data)
+        rub_rate = data_eur['eur'].get('rub')
+        date = data_eur['date']
 
-        print(f"Полный путь: {clean_file_data}")
+        df = pd.DataFrame([{
+            'currency': 'RUB',
+            'rate': round(rub_rate,2),
+            'start_currency': 'EUR',
+            'date': date,
+        }])
+
+        df.to_parquet(clean_file_data,index=False)
+
+        df_read = pd.read_parquet(f'{clean_file_data}')
+        print(f"Полный путь: {clean_file_data}, \nзначение:\n{df_read}")
 
 
 
